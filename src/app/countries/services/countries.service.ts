594f007215b13572a6cf8ea1ccd9a49f4e5ba778
TrypleM <muras.m.m.3@gmail.com>
2024-01-16 11:06:15 +0100

    fin sección 10

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,43 +1,79 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
-import { Country } from '../interfaces/country';
+import { Country } from '../interfaces/country.interface';
+import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
-  private httpUrl: string = 'https://restcountries.com/v3.1';
+  private apiUrl: string = 'https://restcountries.com/v3.1';
 
-  constructor(private http: HttpClient) { }
+  public cacheStore: CacheStore = {
+    byCapital: { term: '', countries: [] },
+    byCountry: { term: '', countries: [] },
+    byRegion: { region: '', countries: [] },
+  }
+
+  constructor(private http: HttpClient) {
+    this.loadFromLocalStorage();
+  }
+
+  private saveToLocalStorage(): void {
+    localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore));
+  }
+
+  private loadFromLocalStorage(): void {
+    if (!localStorage.getItem('cacheStore')) return;
+    this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!);
+  }
+
+  private getCountriesRequest(searchType: string, term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/${searchType}/${term}`;
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError( () => of([])),
+      tap( countries => {
+        switch (searchType) {
+          case 'capital':
+            this.cacheStore.byCapital = { term, countries };
+            break;
+          case 'name':
+            this.cacheStore.byCountry = { term, countries };
+            break;
+          case 'region':
+            this.cacheStore.byRegion = { region: term as Region, countries}
+            break;
+        }
+      }),
+      tap(() => this.saveToLocalStorage())
+      // delay(2000)
+    );
+  }
 
   searchCountryByAlphaCode(code: string): Observable<Country | null>{
-    const url = `${this.httpUrl}/alpha/${code}`;
+    const url = `${this.apiUrl}/alpha/${code}`;
     return this.http.get<Country[]>(url)
     .pipe(
       map( countries => countries.length > 0 ? countries[0] : null),
-      catchError( error => of(null))
+      catchError( () => of(null))
     );
   }
 
   searchCapital(term: string): Observable<Country[]> {
-    return this.search('capital', term);
+    return this.getCountriesRequest('capital', term);
   }
 
   searchCountry(term: string): Observable<Country[]> {
-    return this.search('name', term);
+    return this.getCountriesRequest('name', term);
   }
 
   searchRegion(term: string): Observable<Country[]> {
-    return this.search('region', term);
+    return this.getCountriesRequest('region', term);
   }
 
-  private search(searchType: string, term: string): Observable<Country[]> {
-    const url = `${this.httpUrl}/${searchType}/${term}`;
-    return this.http.get<Country[]>(url)
-    .pipe(
-      catchError( error => of([]))
-    );
-  }
+
 
 }
